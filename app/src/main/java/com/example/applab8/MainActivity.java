8330a0521cf1d3a813f11a221319c523dbf77400
JokerHuang9527 <huangpw1031@gmail.com>
2023-11-24 13:34:31 +0800

    master

---
+++ app/src/main/java/com/example/applab8/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.applab8;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.applab8.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(  MainActivity.this, MyService.class);
+                startService(intent);
+                Toast.makeText( MainActivity.this, "啟動Service", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
+
